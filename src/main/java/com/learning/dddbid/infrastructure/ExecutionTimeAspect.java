3f4f1b96595b105b5fb67c42a99919d67f24eade
meixuesong <meixuesong@gmail.com>
2018-01-14 22:23:44 +0800

    [Add performance log] change to log, instead of system.out

--- src/main/java/com/learning/dddbid/infrastructure/ExecutionTimeAspect.java
+++ src/main/java/com/learning/dddbid/infrastructure/ExecutionTimeAspect.java
@@ -3,11 +3,14 @@ package com.learning.dddbid.infrastructure;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class ExecutionTimeAspect {
+    private static final Logger logger = LoggerFactory.getLogger(ExecutionTimeAspect.class);
 
     @Around("@annotation(LogExecutionTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -17,7 +20,7 @@ public class ExecutionTimeAspect {
 
         long executionTime = System.currentTimeMillis() - start;
 
-        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        logger.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
         return proceed;
     }
 }
